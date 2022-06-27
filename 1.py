acf116e5d7bda739e2c8395c24bd5fe5c42c177f
Marcos Junior <108245405+marcoscacojr@users.noreply.github.com>
2022-06-27 01:14:09 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+x = int(input("Quantidade de latas 350ml: "))
+y = int(input("Quantidade de garrafas 600ml: "))
+z = int(input("Quantidade de garrafas 2L: "))
+k = (350*x) + (600*y) + (2000*z)
+p = k/1000
+print ("Há no total", p,"litros de refrigerante")
